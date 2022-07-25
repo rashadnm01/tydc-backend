466482ec2f46172440cc02e003360bc75664e959
Rashad M <rashadnm01@gmail.com>
2022-07-25 11:31:08 -0500

    Updated .env variables

--- routes/users/users.js
+++ routes/users/users.js
@@ -6,6 +6,29 @@ const User = require("../../models/user");
 router.get("/", (req, res) => {
   res.send("all users will be seen ehere");
 });
+router.get("/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = User.findById(id);
+    res.status(200).send(user);
+  } catch (err) {
+    res.status(404).send("You are unable to find user: " + err);
+  }
+});
+router.post("/delete", async (req, res) => {
+  try {
+    await User.findOneAndDelete({ username: req.body.username });
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+router.put("/update", async (req, res) => {
+  try {
+    await User.findOneAndUpdate({ username: req.body, username }, req.body);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
 
 router.post("/signup", async (req, res) => {
   try {
@@ -30,7 +53,6 @@ router.post("/login", async (req, res) => {
   try {
     const username = req.body.username;
     const password = req.body.password;
-
     const user = await User.findOne({ username });
 
     if (user) {
